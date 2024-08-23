56380219b9b0915bd63a4f2a85a81ae016bcfcdf
bradragz <ragonesebradley@gmail.com>
2024-08-22 20:14:17 -0700

    Add seed data to populate database

--- seeds/seed.js
+++ seeds/seed.js
@@ -1,1 +1,36 @@
+const sequelize = require('../config/connection');
+const { User, Post, Comment } = require('../models');
+
+const userData = require('./userData.json');
+const projectData = require('./projectData.json');
+
+const seedDatabase = async () => {
+  await sequelize.sync({ force: true });
+
+  const users = await User.bulkCreate(userData, {
+    individualHooks: true,
+    returning: true,
+  });
+
+  const posts = await Post.bulkCreate(projectData);
+
+  const comments = [
+    {
+      comment_text: 'Nice!',
+      user_id: 1,
+      post_id: 1,
+    },
+    {
+      comment_text: 'Thanks for the Motivation',
+      user_id: 2,
+      post_id: 2,
+    },
+  ];
+
+  await Comment.bulkCreate(comments);
+
+  process.exit(0);
+};
+
+seedDatabase();
 
