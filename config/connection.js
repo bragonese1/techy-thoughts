80adbf6124c3280ea677b63352a74cb6065454cc
bradragz <ragonesebradley@gmail.com>
2024-08-24 23:48:46 -0700

    edit connection.js for render

--- config/connection.js
+++ config/connection.js
@@ -1,21 +1,17 @@
 const Sequelize = require('sequelize');
 require('dotenv').config();
 
-let sequelize;
-
-if (process.env.DB_URL) {
-  sequelize = new Sequelize(process.env.DB_URL);
-} else {
-  sequelize = new Sequelize(
+const sequelize = new Sequelize(
     process.env.DB_NAME,
     process.env.DB_USER,
     process.env.DB_PASSWORD,
     {
-      host: 'localhost',
-      dialect: 'postgres'
+      host: process.env.DB_HOST,
+      dialect: 'postgres',
+      port: process.env.DB_PORT || 5432,
+      logging: false,
     }
   );
-}
 
 module.exports = sequelize;
 
